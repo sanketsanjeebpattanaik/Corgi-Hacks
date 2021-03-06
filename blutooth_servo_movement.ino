d5ddf39795bfc56274454b6ec0def2900c65537b
Sanket Sanjeeb Pattanaik <53606569+sanketsanjeebpattanaik@users.noreply.github.com>
2021-03-07 02:07:05 +0530

    Updated code

--- blutooth_servo_movement.ino
+++ blutooth_servo_movement.ino
@@ -1,21 +1,22 @@
 #include <Servo.h> 
-
 int servoPin1 = 5; 
 int servoPin2 = 6; 
 
 Servo Servo1,Servo2;
 
-void setup() { 
-   Serial.begin(9600);
-   Servo1.attach(servoPin1);
-   Servo2.attach(servoPin2);
+void setup() {
+  Serial.begin(9600);
+  Servo1.attach(servoPin1);
+  Servo2.attach(servoPin2);
+  
+
 }
+
 
 char c;
 String voice;
 
 void loop() {
-
   if (Serial.available()>0)
   {
     voice="";
@@ -23,25 +24,23 @@ void loop() {
     Serial.print(voice+'\n');
   }
 
-  if(voice=="hello")
+  if(voice=="start")
   {
     Servo1.write(90);
-    delay(1000);
+    Servo2.write(90);
+  } else if(voice=="stop")
+  {
+    Servo1.write(0);
+    Servo2.write(0);
+  }
+
+if(voice=="hello")
+  {
+    Servo1.write(90);
+    Servo2.write(90);
   } else if(voice=="bye")
   {
     Servo1.write(0);
-    delay(1000);
-  }
-
-    if(voice=="start")
-  {
-    Servo2.write(90);
-    delay(1000);
-  } else if(voice=="stop")
-  {
     Servo2.write(0);
-    delay(1000);
   }
-
-
 }
